0b1f8c6a23ba76ac6d7f50c2eba74756cea42508
Raymond Lee <raymond.lee@clover.com>
2017-10-26 13:55:47 -0700

    Fixed comparison and logical operators.

--- node.js/webpay.js
+++ node.js/webpay.js
@@ -30,7 +30,7 @@ var options = {
 };
 
 request(options, function(error, response, body) {
-    if (!error & response.statusCode == 200) {
+    if (!error && response.statusCode === 200) {
         processEncryption(JSON.parse(body));
     }
 });
@@ -79,7 +79,7 @@ function postPayment(cardEncrypted) {
     };
 
     request(options, function(error, response, body) {
-        if (!error & response.statusCode == 200) {
+        if (!error && response.statusCode === 200) {
             console.log(response);
         }
     });
