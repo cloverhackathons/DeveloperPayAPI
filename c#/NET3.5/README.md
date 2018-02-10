175b24053bfa2ccc94a6f86aa28adc0551c59ab6
Raymond Lee <raymond.lee@clover.com>
2018-02-09 16:22:30 -0800

    Added C# .NET 3.5 Example

---
+++ c#/NET3.5/README.md
@@ -0,0 +1,13 @@
+### C# .NET 3.5 Developer Pay API Example
+
+Requires: 
+	Json.NET
+	BigInteger Class from https://www.codeproject.com/Articles/2728/C-BigInteger-Class
+
+To run:
+
+1. Open the DeveloperPayAPI/DeveloperPayAPI.csproj file from Visual Studio.
+
+2. Open `DeveloperPayAPI/WebPay.cs` and edit the file to include your own test Merchant ID, an Order ID belonging to that Merchant, and an OAuth token with PROCESS_CREDIT_CARDS permission.
+
+3. Click on the run button.
---
+++ c#/NET3.5/WebPay.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Security.Cryptography;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace DeveloperPayAPI
+{
+    public class WebPay
+    {
+        private const string merchantID = ""; // Enter your merchant ID here.
+        private const string orderID = ""; // Enter your order ID here.
+        private const string apiToken = ""; // Enter your API Token here.
+
+        private const string targetEnv = "https://sandbox.dev.clover.com/v2/merchant/";
+
+        private const long amount = 100;
+        private const long tipAmount = 0;
+        private const long taxAmount = 0;
+        private const string cardNumber = "4761739001010010";
+        private const string expMonth = "12";
+        private const string expYear = "2018";
+        private const string cvv = "123";
+
+        /**
+         *  POST to /v2/merchant/{mId}/pay with the encrypted card data.
+         * 
+         *  <param name="encryptedCard">The encrypted card data.</param>
+         */
+        private static void postPayment(string encryptedCard)
+        {
+            string posturl = targetEnv + merchantID + "/pay";
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(posturl);
+            request.Method = "POST";
+            request.ContentType = "application/json";
+            request.Headers.Add("Authorization", "Bearer " + apiToken);
+
+            Dictionary<string, string> postData = new Dictionary<string, string>();
+            postData.Add("orderId", orderID);
+            postData.Add("currency", "usd");
+            postData.Add("amount", amount.ToString());
+            postData.Add("tipAmount", tipAmount.ToString());
+            postData.Add("taxAmount", taxAmount.ToString());
+            postData.Add("expMonth", expMonth);
+            postData.Add("expYear", expYear);
+            postData.Add("cvv", cvv);
+            postData.Add("cardEncrypted", encryptedCard);
+            postData.Add("last4", cardNumber.Substring(cardNumber.Length - 4));
+            postData.Add("first6", cardNumber.Substring(0, 6));
+
+            string json = JsonConvert.SerializeObject(postData, Formatting.Indented);
+
+            Console.WriteLine(json);
+
+            StreamWriter streamWriter = new StreamWriter(request.GetRequestStream());
+
+            streamWriter.Write(json);
+            streamWriter.Flush();
+            streamWriter.Close();
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            StreamReader readStream = new StreamReader(response.GetResponseStream());
+            string stringResult = readStream.ReadToEnd().ToString();
+
+            if ((int)response.StatusCode >= 200 && (int)response.StatusCode <= 299)
+            {
+                Console.WriteLine(stringResult);
+            }
+
+            else
+            {
+                Console.WriteLine("{0} ({1})", (int)response.StatusCode, stringResult);
+            }
+        }
+
+
+
+        public static void Main(string[] args)
+        {
+            string url = targetEnv + merchantID + "/pay/key";
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "GET";
+            request.Headers.Add("Authorization", "Bearer " + apiToken);
+
+            // GET to /v2/merchant/{mId}/pay/key 
+            // To get the encryption information needed for the pay endpoint.
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            StreamReader readStream = new StreamReader(response.GetResponseStream());
+            string stringResult = readStream.ReadToEnd().ToString();
+
+            if ((int)response.StatusCode >= 200 && (int)response.StatusCode <= 299)
+            {
+                Dictionary<string, string> jsonResult =
+                    JsonConvert.DeserializeObject<Dictionary<string, string>>(stringResult);
+
+                string modulus = jsonResult["modulus"];
+                string exponent = jsonResult["exponent"];
+                string prefix = jsonResult["prefix"];
+
+                byte[] modulusBytes = new BigInteger(modulus, 10).getBytes();
+                byte[] exponentBytes = new BigInteger(exponent, 10).getBytes();
+
+                RSAParameters rsaParams = new RSAParameters {
+                    Modulus = modulusBytes,
+                    Exponent = exponentBytes
+                };
+
+                RSACryptoServiceProvider rsa = new RSACryptoServiceProvider();
+                rsa.ImportParameters(rsaParams);
+
+                byte[] encryptedCard = rsa.Encrypt(Encoding.UTF8.GetBytes(prefix + cardNumber), true);
+
+                string base64EncryptedCard = Convert.ToBase64String(encryptedCard);
+
+                postPayment(base64EncryptedCard);
+            }
+
+            else
+            {
+                Console.WriteLine("{0} ({1})", (int)response.StatusCode, stringResult);
+            }
+        }
+    }
+}
