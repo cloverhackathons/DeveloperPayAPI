50a7960ed9074f4a0756b4807a3a151829ad4507
Frank Faustino <nawzyah@gmail.com>
2019-05-24 13:01:07 -0700

    Add Xcode workspace files

---
+++ swift/DeveloperPay/DeveloperPay.h
@@ -0,0 +1,19 @@
+//
+//  DeveloperPay.h
+//  DeveloperPay
+//
+//  Created by Frank Faustino on 5/25/19.
+//  Copyright © 2019 Clover Network, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DeveloperPay.
+FOUNDATION_EXPORT double DeveloperPayVersionNumber;
+
+//! Project version string for DeveloperPay.
+FOUNDATION_EXPORT const unsigned char DeveloperPayVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DeveloperPay/PublicHeader.h>
+
+
